84f93aa021a2eb0acc603ecf19c3e6dad47c71ea
jm-moore <jmmoore9358@gmail.com>
2022-08-22 10:52:10 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,5 @@
+﻿using StaticExercise;
+
+Console.WriteLine(TempConverter.FahrenheitToCelsius(75));
+
+Console.WriteLine(TempConverter.CelsiusToFahrenheit(27));
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double number)
+        {
+            var number2 = number - 32;
+            return number2 *= .5556;
+        }
+
+        public static double CelsiusToFahrenheit(double number)
+        {
+            return number * 1.8 + 32;
+        }
+    }
+}
